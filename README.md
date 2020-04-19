10678ae46c3e31e440255639438236dde1679926
Ahmed Bule <ahmedbule01@gmail.com>
2020-04-18 22:33:17 -0500

    added README.md and fixed some of the errors in the for loop

---
+++ README.md
@@ -0,0 +1,11 @@
+In this project, I created an application that generates a random password based on user-selected criteria. 
+
+I have created three files. index.html, style.css, and script.js
+
+In my Index.html, I created a form ID and names it passwordGenerator. Thereafter, I had an input number and with max and min. From there on, there were labels for IDs, such as includeUppercase, includeLowercase, and includeSymbols. 
+
+
+As for the Javascript, I started with DOM and getElementById for all the IDs that I mentioned in the index.html file. Afterwards, I had variables, such as validUpper and validLower, and then I gave a value for each name. 
+
+The addEventListener() method attaches an event handler to the specified element.The math.random returns a random interger and Math.floor rounds a number, variable, or result down. 
+
--- script.js
+++ script.js
@@ -1,17 +1,59 @@
-var characterAmountNumber = document.getElementById
-("characterAmountNumber")
+var characterAmountNumber = document.getElementById("characterAmountNumber")
+var includeUppercase = document.getElementById("includeUppercase")
+var includeNumbers = document.getElementById("includeNumbers")
+var includelowercase = document.getElementById("includelowercase")
+var includeSymbols = document.getElementById("includeSymbols")
+var includebuttons = document.getElementById("includebuttons")
+var passworddisplay = document.querySelector(".password-display")
+var validUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var validLower = "abcdefghijklmnopqrstuvwxyz"
+var validNumber = "0123456789"
+var validSymbols = "!@#$%^&*"
+var acceptcharacter = []
 
-var form = document.getElementById("passwordGenerator")
-characterAmountNumber.addEventListener("input", synchCharacterAmount)
-
-form.addEventListener("submit", f => {
+console.log("JS file loaded")
+// start with addEventListener, so that you can hear the click and then have a function. 
+includebuttons.addEventListener("click", function (f) {
     f.preventDefault()
-    var characterAmountNumber = characterAmountNumber.value
-    var password = generatePassword(characterAmountNumber, IncludeNumbers, IncludeUppercase, IncludeSymbols)
-})
+ // By having the preventDefault, we can prevent the browser default behavior.    
+    passworddisplay.innerHTML = ""
+    acceptcharacter = []
+    generatePassword = ""
+    if (parseInt(characterAmountNumber.value) > 8 && parseInt(characterAmountNumber.value) < 128) {
+        if (includeUppercase.checked) {
+            acceptcharacter += validUpper
+        }
+        if (includelowercase.checked) {
+            acceptcharacter += validLower
+        }
+        if (includeSymbols.checked) {
+            acceptcharacter += validSymbols
+        }
+        if (includeNumbers.checked) {
+            acceptcharacter += validNumber
+        }
+        console.log(acceptcharacter)
+        
+        if (acceptcharacter.length > 0) {
+            var generatePassword = ""
+            for (var i = 0; i < parseInt(characterAmountNumber.value); i++) { // I use the for loop because it looks through a block of code a number of times and it is the simplest one. 
+                var index = Math.floor(Math.random() * acceptcharacter.length)
+                generatePassword += acceptcharacter[index]
+            // The math.random returns a random interger and Math.floor rounds a number, variable, or result down.    
+            }
+            // By having a console.log, it helps to view if you generated a password or not. 
+            console.log(generatePassword)
+            passworddisplay.innerHTML = generatePassword
+        }
+        else {      // Here, we are alerting the user to choose some of the option. Unfortunately, we will not be able to generate a password without choosing at least one option. 
+            alert("Please choose any of the options")
+        }
+    }else {
+        alert ("invalid character length")
+    }
+
+    })
 
 
-function synchCharacterAmount(){
-    var value = target.value 
-    characterAmountNumber.value = value 
-}
+
+
